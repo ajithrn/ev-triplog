5cc874e4c5c79fc8886645ce27a291e3a48f14fa
Ajith R N <dev@ajithrn.com>
2025-11-10 19:10:09 +0530

    refactor: Remove unused static path generation for trip and vehicle pages

--- app/trips/[id]/page.tsx
+++ app/trips/[id]/page.tsx
@@ -1,12 +1,5 @@
 import TripDetailsClient from './TripDetailsClient';
 
-// For static export, we need to generate at least one static path
-// The actual routing will be handled client-side
-export async function generateStaticParams() {
-  // Generate a placeholder path - actual IDs will be handled client-side
-  return [{ id: 'placeholder' }];
-}
-
 export default function TripDetailsPage() {
   return <TripDetailsClient />;
 }
--- app/vehicles/[id]/edit/page.tsx
+++ app/vehicles/[id]/edit/page.tsx
@@ -1,12 +1,5 @@
 import EditVehicleClient from './EditVehicleClient';
 
-// For static export, we need to generate at least one static path
-// The actual routing will be handled client-side
-export async function generateStaticParams() {
-  // Generate a placeholder path - actual IDs will be handled client-side
-  return [{ id: 'placeholder' }];
-}
-
 export default function EditVehiclePage() {
   return <EditVehicleClient />;
 }
